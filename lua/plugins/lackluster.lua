1e6ccbb56f0bd9a446ffbabe6966efb8ecdd19b2
haedarrfd <hroofid@gmail.com>
2024-11-01 10:37:44 +0700

    add lackluster colorscheme

---
+++ lua/plugins/lackluster.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "slugbyte/lackluster.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nvim-web-devicons").setup({})
+      vim.cmd.colorscheme("lackluster-hack")
+    end,
+  },
+}
