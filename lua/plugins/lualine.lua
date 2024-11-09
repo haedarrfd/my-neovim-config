544ea5d6868f4153726256c26e7b693f6d447baa
haedarrfd <hroofid@gmail.com>
2024-11-09 20:33:57 +0700

    refactor: change theme and format lualine plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,15 +5,26 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "gruvbox",
+				theme = "codedark",
+				icons_enabled = false,
+				component_separators = "",
+				section_separators = "",
 			},
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
 				lualine_x = { "" },
-				lualine_y = { "encoding" },
+				lualine_y = { "" },
 				lualine_z = { "progress", "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = { "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
 			},
 		})
 	end,
